1c1b4b1c028f529995f162aab2b477f8ff2d52c5
apiraino <apiraino@users.noreply.github.com>
2019-12-02 01:23:38 +0100

    updates

--- get_issues.py
+++ get_issues.py
@@ -16,7 +16,7 @@ GITHUB_REPO = 'rust'
 # GITHUB_OWNER = 'gtk-rs'
 # GITHUB_REPO = 'gtk'
 DEST_FILE = 'issues.csv'
-HEADER = ['ID', 'CREATED_AT', 'UPDATED_AT', 'TITLE', 'AUTHOR', 'REVIEWER', 'URL', 'COMMENT_TS', 'LAST_COMMENT_URL']
+HEADER = ['ID', 'TITLE', 'CREATED_AT', 'UPDATED_AT', 'URL', 'AUTHOR', 'ASSIGNEE/REVIEWER', 'LAST_COMMENT_TS', 'LAST_COMMENT_URL', 'STATUS', 'LABELS']
 
 # https://hackmd.io/i4JxpNcWSWmx73qGwFWX-w
 
@@ -65,12 +65,13 @@ def _make_request(method, url, payload=None):
     return resp
 
 
-def make_issue_url(param_sort='updated#desc', labels=''):
+def make_url(item_type, param_sort='updated#desc', labels=''):
     (sort, direction) = param_sort.split('#')
-    url = 'https://{}/{}/{}/issues?state=open&sort={}&direction={}'.format(
+    url = 'https://{}/{}/{}/{}?state=open&sort={}&direction={}'.format(
         GITHUB_URL,
         GITHUB_OWNER,
         GITHUB_REPO,
+        item_type,
         sort,
         direction
     )
@@ -84,6 +85,10 @@ def make_request(url):
     items = list()
     resp = _make_request('GET', url)
     items = resp.json()
+    # with open('issues_pulls.json', 'w') as fp:
+    #     json.dump(items, fp)
+    # print('done')
+    # sys.exit(0)
     while 'next' in resp.links:
         url = resp.links['next']['url']
         # print('Requesting {}'.format(url))
@@ -92,26 +97,43 @@ def make_request(url):
     return items
 
 
-def parse_issue(issue):
+def parse_issue(issue, item_type):
     author = issue['user']['login']
-    assignee = reviewer = comment_ts = comment_url = None
+    assignee = reviewer = comment_ts = comment_url = status = None
+    labels = []
     if issue.get('assignee'):
         assignee = issue['assignee']['login']
-        comment_ts, reviewer, comment_url = get_last_comment(issue['comments_url'], assignee, author)
-        if not reviewer:
-            reviewer = assignee
+
+        if item_type == 'issues':
+            comment_ts, reviewer, comment_url = get_last_comment(issue['comments_url'], assignee, author)
+            if not reviewer:
+                reviewer = assignee
+
+        if item_type == 'pulls':
+            review_ts, reviewer, comment_url = get_last_comment(issue['review_comments_url'], assignee, author)
+            if not reviewer:
+                reviewer = assignee
+
+    for label in issue['labels']:
+        labels.append(label['name'])
+
+    if item_type == 'pulls':
+        status = get_status_or_empty(issue['statuses_url'])
+
     return {
         'id': issue['number'],
         'title': issue['title'],
         'created_at': issue['created_at'],
         'updated_at': issue['updated_at'],
-        'num_comments': issue['comments'],
-        'state': issue['state'],
+        # 'num_comments': issue['comments'],
+        # 'state': issue['state'],
         'url': issue['html_url'],
         'author': author,
         'reviewer': reviewer,
         'comment_ts': comment_ts,
-        'last_comment_url': comment_url
+        'last_comment_url': comment_url,
+        'status': status,
+        'labels': ', '.join(labels)
     }
 
 
@@ -138,14 +160,25 @@ def get_last_comment(comments_url, assignee, author):
     return (updated_at_str, reviewer, comment_url)
 
 
+def get_status_or_empty(status_url):
+    resp_data = make_request(status_url)
+    if resp_data:
+        # TODO: only the first item?
+        status = resp_data[0]['state']
+    else:
+        status = '-'
+    return status
+
+
 if __name__ == '__main__':
-    print("-- Getting issues from github ...")
     # - filter by S-waiting-on-review
     # - filter by S-waiting-on-author
     labels = ''
     if len(sys.argv) == 2:
         labels = sys.argv[1]
-    issue_url = make_issue_url(labels=labels)
+    item_type = 'pulls'  # or 'issues'
+    print("-- Getting {} from github ...".format(item_type))
+    issue_url = make_url(item_type=item_type, labels=labels)
     issues = make_request(issue_url)
     issues.reverse()
     print('-- Found {} open issues'.format(len(issues)))
@@ -162,12 +195,13 @@ if __name__ == '__main__':
         for issue in issues:
             row = []
             print("-- Got issue #{}".format(issue['number']))
-            # if _count == 10:
-            #     break
-            data = parse_issue(issue)
+            if _count == 10:
+                break
+            data = parse_issue(issue, item_type)
             body = parse_body(issue['body'])
             for fld in data:
                 row.append(str(data[fld]))
             _writer.writerow(row)
             _count += 1
         print("-- {} written".format(DEST_FILE))
+    print('DONE')
